f93f671e4434bb65833369aa41c88b4881dce6b7
qzhou <qiang.zhou@hmcts.net>
2025-10-23 12:16:48 +0100

    Use object id as reference

--- components/xhibit-layer/permissions.tf
+++ components/xhibit-layer/permissions.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_assignment" "blob_landing_data_contributor" {
   scope                = data.azurerm_storage_account.langing_storage.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azuread_group.dlrm_crime_admin.id
+  principal_id         = data.azuread_group.dlrm_crime_admin.object_id
 
 }
