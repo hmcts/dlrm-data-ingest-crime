27596dc82f0d0c24e66ae7c2959f574929cbe916
qzhou <qiang.zhou@hmcts.net>
2025-10-23 14:20:17 +0100

    clean up example file

--- components/xhibit-layer/permissions.tf
+++ components/xhibit-layer/permissions.tf
@@ -4,3 +4,16 @@ resource "azurerm_role_assignment" "blob_landing_data_contributor" {
   principal_id         = data.azuread_group.dlrm_crime_admin.object_id
 
 }
+
+resource "azurerm_role_assignment" "blob_data_reader" {
+  scope                = data.azurerm_storage_account.langing_storage.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = data.azurerm_databricks_access_connector.unity_catalog.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "blob_data_contributor" {
+  scope                = data.azurerm_storage_account.langing_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_databricks_access_connector.unity_catalog.identity[0].principal_id
+
+}
