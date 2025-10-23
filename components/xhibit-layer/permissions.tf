09c1981e814bd6dd4f59fb63de80402a8a13a37d
qzhou <qiang.zhou@hmcts.net>
2025-10-23 11:37:41 +0100

    Fix duplicated name

--- components/xhibit-layer/permissions.tf
+++ components/xhibit-layer/permissions.tf
@@ -1,4 +1,4 @@
-resource "azurerm_role_assignment" "blob_data_contributor" {
+resource "azurerm_role_assignment" "blob_landing_data_contributor" {
   scope                = data.azurerm_storage_account.langing_storage.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = data.azuread_group.dlrm_crime_admin
