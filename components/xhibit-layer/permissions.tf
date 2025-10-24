dcc820f70631f4a770ce4a8a721e9282dfee9f32
qzhou <qiang.zhou@hmcts.net>
2025-10-24 16:59:57 +0100

    Update terraform reference

--- components/xhibit-layer/permissions.tf
+++ components/xhibit-layer/permissions.tf
@@ -17,7 +17,7 @@ resource "azurerm_role_assignment" "blob_data_contributor" {
   principal_id         = data.azurerm_databricks_access_connector.unity_catalog.identity[0].principal_id
 }
 
-resource "azurerm_role_assignment" "blob_data_contributor" {
+resource "azurerm_role_assignment" "curated_brics_data_contributor" {
   scope                = data.azurerm_storage_account.curated_storage.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = data.azurerm_databricks_access_connector.unity_catalog.identity[0].principal_id
