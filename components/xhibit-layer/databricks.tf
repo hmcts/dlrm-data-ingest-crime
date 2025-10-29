caafdd5579e068c86708d2a25d50769bd8cb53dc
qzhou <qiang.zhou@hmcts.net>
2025-10-29 12:17:43 +0000

    move config to variables

--- components/xhibit-layer/databricks.tf
+++ components/xhibit-layer/databricks.tf
@@ -15,11 +15,11 @@ data "databricks_spark_version" "latest_lts" {
 resource "databricks_cluster" "shared_autoscaling" {
   cluster_name            = "Dlrm Crime Shared Autoscaling ${ var.env }"
   spark_version           = data.databricks_spark_version.latest_lts.id
-  node_type_id            = "Standard_DS3_v2"
-  autotermination_minutes = 20
+  node_type_id            = var.dbrics_node_type_id
+  autotermination_minutes = var.dbrics_auto_termination_mins
   autoscale {
-    min_workers = 1
-    max_workers = 20
+    min_workers = var.dbrics_min_workers
+    max_workers = var.dbrics_max_workers
   }
   data_security_mode      = "USER_ISOLATION"
   custom_tags             = local.common_tags
--- components/xhibit-layer/inputs-optional.tf
+++ components/xhibit-layer/inputs-optional.tf
@@ -18,3 +18,27 @@ variable "project" {
   description = "The name of the data migration project."
   default     = "Crime Legacy Migration"
 }
+
+variable "dbrics_node_type_id" {
+  type        = string
+  description = "Databricks node type"
+  default     = "Standard_DS3_v2"
+}
+
+variable "dbrics_auto_termination_mins" {
+  type        = number
+  description = "Databricks auto termination in minutes"
+  default     = 30
+}
+
+variable "dbrics_min_workers" {
+  type        = number
+  description = "Databricks minimun number of workers"
+  default     = 1
+}
+
+variable "dbrics_max_workers" {
+  type        = number
+  description = "Databricks maximun number of workers"
+  default     = 15
+}
