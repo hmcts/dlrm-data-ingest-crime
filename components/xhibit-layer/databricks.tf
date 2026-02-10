745d16fdc5b303a57ecad392f8409e649b1b515a
qzhou <qiang.zhou@hmcts.net>
2026-02-10 14:26:42 +0000

    Update catalogue configuration

--- components/xhibit-layer/databricks.tf
+++ components/xhibit-layer/databricks.tf
@@ -76,17 +76,17 @@ resource "databricks_storage_credential" "external" {
   azure_managed_identity {
     access_connector_id = data.azurerm_databricks_access_connector.unity_catalog.id
   }
-  # isolation_mode = "ISOLATION_MODE_ISOLATED"
+  isolation_mode = "ISOLATION_MODE_ISOLATED"
   comment = "Managed by TF"
 }
 
-# resource "databricks_external_location" "landing_external" {
-#   name = "external_st_${ var.env }"
-#   url = format("abfss://%s@%s.dfs.core.windows.net", var.landing_container, data.azurerm_storage_account.langing_storage.name)
-#   credential_name = databricks_storage_credential.external.id
-#   comment         = "Managed by TF"
-#   # isolation_mode = "ISOLATION_MODE_ISOLATED"
-# }
+resource "databricks_external_location" "landing_external" {
+  name = "external_st_${ var.env }"
+  url = format("abfss://%s@%s.dfs.core.windows.net", var.landing_container, data.azurerm_storage_account.langing_storage.name)
+  credential_name = databricks_storage_credential.external.id
+  comment         = "Managed by TF"
+  isolation_mode = "ISOLATION_MODE_ISOLATED"
+}
 
 # resource "databricks_grants" "storage_cred_grants" {
 #   storage_credential = databricks_storage_credential.external.id
