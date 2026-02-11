fc2b5c406063b00b2b8954ef0d2ff2d81744f3b5
qzhou <qiang.zhou@hmcts.net>
2026-02-11 14:35:27 +0000

    amend name

--- components/xhibit-layer/databricks.tf
+++ components/xhibit-layer/databricks.tf
@@ -96,7 +96,7 @@ resource "databricks_storage_credential" "external" {
 }
 
 resource "databricks_external_location" "landing_external" {
-  name = "external_st_${ var.env }"
+  name = "external_st"
   url = format("abfss://%s@%s.dfs.core.windows.net", var.landing_container, data.azurerm_storage_account.langing_storage.name)
   credential_name = databricks_storage_credential.external.id
   comment         = "Managed by TF"
