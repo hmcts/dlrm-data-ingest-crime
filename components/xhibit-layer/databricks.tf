1864dccc89e8e6c08494f37aeae6aff9c9aaf9cb
qzhou <qiang.zhou@hmcts.net>
2026-03-05 09:51:52 +0000

    Update group reference

--- components/xhibit-layer/databricks.tf
+++ components/xhibit-layer/databricks.tf
@@ -77,11 +77,11 @@ resource "databricks_permissions" "shared_autoscaling" {
         permission_level = "CAN_ATTACH_TO"
     }
     access_control {
-      group_name       = databricks_group.crime_users.display_name
+      group_name       = data.databricks_group.crime_users.display_name
       permission_level = "CAN_RESTART"
     }
     access_control {
-      group_name       = databricks_group.crime_admins.display_name
+      group_name       = data.databricks_group.crime_admins.display_name
       permission_level = "CAN_MANAGE"
     }
 }
