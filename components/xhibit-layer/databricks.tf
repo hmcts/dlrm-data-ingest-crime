ea57f05bbb4140966e37f7aa067a8e2b2ca573d1
willforbes <william.forbes@solirius.com>
2026-07-16 17:55:40 +0100

    Revert "Adding privileges to the crime_system catalog (#34)"
    This reverts commit 24c27a3a51369f20e76998a33590e9254a310dc3.
    
    Remove permissions on crime_system

--- components/xhibit-layer/databricks.tf
+++ components/xhibit-layer/databricks.tf
@@ -156,54 +156,12 @@ resource "databricks_grants" "schema_raw_external_grants" {
   }
 }
 
-resource "databricks_grants" "crime_system_catalog_grants" {
-  catalog = "crime_system"
-
-  grant {
-    principal  = data.databricks_group.crime_admins.display_name
-    privileges = ["ALL_PRIVILEGES", "MANAGE"]
-  }
-
-  grant {
-    principal  = data.databricks_group.crime_users.display_name
-    privileges = ["USE_CATALOG"]
-  }
-}
-
-resource "databricks_grants" "crime_system_schema_grants" {
-  schema = "crime_system.default"
-
-  grant {
-    principal  = data.databricks_group.crime_admins.display_name
-    privileges = ["ALL_PRIVILEGES", "MANAGE"]
-  }
-
-  grant {
-    principal  = data.databricks_group.crime_users.display_name
-    privileges = ["USE_SCHEMA"]
-  }
-}
-
-resource "databricks_grants" "crime_system_volume_grants" {
-  volume = "crime_system.default.artifacts"
-
-  grant {
-    principal  = data.databricks_group.crime_admins.display_name
-    privileges = ["ALL_PRIVILEGES", "MANAGE"]
-  }
-
-  grant {
-    principal  = data.databricks_group.crime_users.display_name
-    privileges = ["READ_FILES"]
-  }
-}
-
 resource "databricks_artifact_allowlist" "crime_artifacts" {
   count         = var.assign_account == "true" ? 1 : 0
   metastore_id  = data.databricks_metastore.this[0].id
   artifact_type  = "LIBRARY_JAR"
   artifact_matcher {
-    match_type = "PREFIX_MATCH"
+    match_type = "PREFIX_MATCH" 
     artifact = "/Volumes/crime_system/default/artifacts/"
   }
 }
